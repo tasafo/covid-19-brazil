4b26940a86871d77e5bef383c5afe115109e9324
Luiz Sanches <lsanches@protonmail.com>
2020-04-11 01:43:00 -0300

    Correção da contagem de dias sem dados

--- app/models/cases_by_date.rb
+++ app/models/cases_by_date.rb
@@ -14,31 +14,32 @@ class CasesByDate
 
     today = Date.parse(Time.zone.now.strftime('%Y-%m-%d'))
 
-    initial_date = CasesByDate.count.zero? ? Date.parse('2020-02-25') : today
+    initial_date = CasesByDate.count.zero? ? Date.parse('2020-02-25') : today - 1
 
     date_range = (initial_date..today).map { |d| d.strftime('%Y-%m-%d') }
 
     date_range.each_with_index do |date, index|
       cases = covid.by_date(date)
 
-      cases = covid.by_date(date_range[index - 1]) if cases.empty?
+      count = 0
+      while cases.empty? do
+        cases = covid.by_date(date_range[index - count += 1])
+      end
 
-      unless cases.empty?
-        cases_by_date = CasesByDate.find_by(date: date)
+      cases_by_date = CasesByDate.find_by(date: date)
 
-        total = cases.sum { |h| h['cases'] }
-        deaths = cases.sum { |h| h['deaths'] }
-        suspects = cases.sum { |h| h['suspects'] }
+      total = cases.sum { |h| h['cases'] }
+      deaths = cases.sum { |h| h['deaths'] }
+      suspects = cases.sum { |h| h['suspects'] }
 
-        updated_params = { cases: total, suspects: suspects, deaths: deaths }
+      updated_params = { cases: total, suspects: suspects, deaths: deaths }
 
-        created_params = { date: date }
+      created_params = { date: date }
 
-        if cases_by_date.present?
-          cases_by_date.update!(updated_params)
-        else
-          CasesByDate.create!(created_params.merge(updated_params))
-        end
+      if cases_by_date.present?
+        cases_by_date.update!(updated_params)
+      else
+        CasesByDate.create!(created_params.merge(updated_params))
       end
     end
   end
